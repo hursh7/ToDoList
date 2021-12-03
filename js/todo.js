f6d67dde1a8333568c525c2f8afac864f5f4fe66
junhp7 <junhp1994@gmail.com>
2021-12-03 17:19:08 +0900

    add array to localStorage

--- js/todo.js
+++ js/todo.js
@@ -4,6 +4,16 @@ const toDoForm = document.querySelector('#todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.querySelector('.todo-list');
 
+const TODOS_KEY = 'toDos'
+
+let toDoItem = []; //  parsedToDos으로 로컬 스토리지에 새로운 list 저장 하고, 새로운 list를 추가해줄 때 기존의 list를 덮어쓰는 걸 방지하기 위해 let으로 변수 선언해주고, parsedToDos값을 넣어준 후, forEach 메소드를 돌린다.
+
+// 로컬 스토리지는 string만 저장할 수 있다.
+
+function saveToDos() { // toDos array를 로컬 스토리지에 저장하는 함수
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDoItem)); 
+};
+
 function deleteToDo(event) {
     const li = event.target.parentElement; // button의 부모인 li를 찾아서 삭제
     li.remove();
@@ -25,7 +35,22 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDoItem.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 };
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDoItem = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+// forEach는 배열에 있는 각각의 아이템이 함수를 실행하도록 해주는 메서드 이다.
+
+// JSON.stringify는 object나 array를 string으로 변환 해준다.
+// JSON.parse는 string을 object나 array로 변환 해준다. 
