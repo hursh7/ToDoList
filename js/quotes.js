386c5344c1975e75c423a72c27ca01ee4a02cc66
junhp7 <junhp1994@gmail.com>
2021-12-07 16:32:18 +0900

    add style with scss

--- js/quotes.js
+++ js/quotes.js
@@ -2,29 +2,33 @@
 
 const quotes = [
     {
-        quote: 'Lorem ipsum dolor1',
-        author: 'Tony'
+        quote: '서두르지 말되 멈추지 말라',
+        author: '-괴테-'
     },
     {
-        quote: 'Lorem ipsum dolor2',
-        author: 'Eden Hazard'
+        quote: '생각하고 살지 않으면 사는 대로 생각하게 된다.',
+        author: '-르네 데카르트-'
     },
     {
-        quote: 'Lorem ipsum dolor3',
-        author: 'Lionel Messi'
+        quote: '과거에서 교훈을 얻을 수는 있어도, 과거 속에서 살 수는 없다.',
+        author: '-린든 B. 존슨-'
     },
     {
-        quote: 'Lorem ipsum dolor4',
-        author: 'Alexis Sanchez'
+        quote: '길을 아는 것과 그 길을 걷는 것은 분명히 다르다.',
+        author: '-모피어스-'
     },
     {
-        quote: 'Lorem ipsum dolor5',
-        author: 'Theo'
+        quote: '처음에는 우리가 습관을 만들지만, 그 다음에는 습관이 우리를 만든다.',
+        author: '-존 드라이든-'
+    },
+    {
+        quote: '낭비한 시간에 대한 후회는 더 큰 시간 낭비이다.',
+        author: '-메이슨 쿨리-'
     },
 ]
 
-const quote = document.querySelector('.quote span:first-child');
-const author = document.querySelector('.quote span:last-child');
+const quote = document.querySelector('.quote p:first-child');
+const author = document.querySelector('.quote p:last-child');
 Math.floor(Math.random() * 10)
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
--- js/weather.js
+++ js/weather.js
@@ -10,8 +10,8 @@ function onGeoSuccess(position) {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        const weather = document.querySelector('#weather span:first-child');
-        const city = document.querySelector('#weather span:last-child');
+        const weather = document.querySelector('.weather span:first-child');
+        const city = document.querySelector('.weather span:last-child');
         city.innerText = data.name; 
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
